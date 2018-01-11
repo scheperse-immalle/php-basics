47340bc5f62471bd807a94f61743a82ba71714df
scheperse-immalle <eline.schepers@immalle.be>
2018-01-11 10:00:24 +0100

    add existing file

--- 07_switch_scrabble.php
+++ 07_switch_scrabble.php
@@ -1,7 +1,7 @@
 <?php
 
 $woord = "quotering";
-$punten = 0;
+$points=0;
 
 // gebruik een switch-case-statement binnen een for-loop om
 // te itereren over een woord en de Scrabble-punten ervan te berekenen
@@ -15,5 +15,38 @@ $punten = 0;
 // I 1 punt
 // N 1 punt
 
+for($i=0;$i<strlen($woord);$i++)
+{
+	switch($woord[$i])
+	{
+		case "q":
+			$points +=10;
+			break;
+		case "u":
+			$points +=4;
+			break;
+		case "o":
+			$points +=1;
+			break;
+		case "t":
+			$points +=2;
+			break;
+		case "e":
+			$points +=1;
+			break;
+		case "r":
+			$points +=2;
+			break;
+		case "i":
+			$points +=1;
+			break;
+		case "n":
+			$points +=1;
+			break;
+	
+	}
+
+}
+echo $points;
 
 ?>
